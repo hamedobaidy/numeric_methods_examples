ef8f3cf16864833c7f84b3facabbd029297f5e3c
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-30 00:10:33 +0430

    systems_root_finding

---
+++ numeric_examples/root_finding/systems_root_finding.cpp
@@ -0,0 +1,10 @@
+/*
+ * systems_root_finding.cpp
+ *
+ *  Created on: Jul 30, 2015
+ *      Author: hamed
+ */
+
+
+
+
---
+++ numeric_examples/root_finding/systems_root_finding.h
@@ -0,0 +1,15 @@
+/*
+ * systems_root_finding.h
+ *
+ *  Created on: Jul 30, 2015
+ *      Author: hamed
+ */
+
+#ifndef SYSTEMS_ROOT_FINDING_H_
+#define SYSTEMS_ROOT_FINDING_H_
+
+
+
+
+
+#endif /* SYSTEMS_ROOT_FINDING_H_ */
