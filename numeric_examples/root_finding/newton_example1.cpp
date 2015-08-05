55b3e54b9ae42814e82b0a17bed76736eabc8490
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-05 18:17:42 +0430

    newton_example1

---
+++ numeric_examples/root_finding/newton_example1.cpp
@@ -0,0 +1,16 @@
+/*
+ * newton_example1.cpp
+ *
+ *  Created on: Aug 5, 2015
+ *      Author: hamed
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	return 0;
+}
+
